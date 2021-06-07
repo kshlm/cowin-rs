e95ebc86998208f2a354052840a8d1a64970e4e2
Kaushal M <kaushal.madappa@dominodatalab.com>
2021-06-07 20:31:12 +0530

    Begin a proper CLI tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use clap::Clap;
 use color_eyre::eyre::Result;
 
 use cowin_rs::api::location::StatesAndDistricts;
@@ -6,8 +7,32 @@ use cowin_rs::api::location::StatesAndDistricts;
 async fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let sd = StatesAndDistricts::get().await?;
-    dbg!(sd);
-
+    let app = App::parse();
+    match app.subcmd {
+        SubCommand::DistrictId(d) => {
+            let sd = StatesAndDistricts::get().await?;
+            println!("{}", sd.district_id(d.state.as_str(), d.district.as_str())?);
+        }
+    }
     Ok(())
+}
+
+#[derive(Clap, Debug)]
+#[clap(name = "cowin-rs")]
+struct App {
+    #[clap(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Clap, Debug)]
+enum SubCommand {
+    DistrictId(DistrictId),
+}
+
+#[derive(Clap, Debug)]
+struct DistrictId {
+    #[clap(long, index(1))]
+    state: String,
+    #[clap(long, index(2))]
+    district: String,
 }
