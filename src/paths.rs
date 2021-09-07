fd89aed7be6650b8dafd5ee3b42085a167e57cfd
Kaushal M <kshlmster@gmail.com>
2021-09-07 11:52:22 +0530

    s/lazy_static/once_cell/

--- src/paths.rs
+++ src/paths.rs
@@ -1,14 +1,12 @@
 use std::{fs, path::PathBuf};
 
 use directories::ProjectDirs;
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 
-lazy_static! {
-    pub(crate) static ref PROJECT_DIRS: ProjectDirs =
-        ProjectDirs::from("in", "kshlm", "cowin").unwrap_or_else(|| unreachable!());
-    pub(crate) static ref CACHE: PathBuf = {
-        let cache = PROJECT_DIRS.cache_dir();
-        fs::create_dir_all(cache).expect("Failed to create cache dir");
-        cache.to_path_buf()
-    };
-}
+pub(crate) static PROJECT_DIRS: Lazy<ProjectDirs> =
+    Lazy::new(|| ProjectDirs::from("in", "kshlm", "cowin").unwrap_or_else(|| unreachable!()));
+pub(crate) static CACHE: Lazy<PathBuf> = Lazy::new(|| {
+    let cache = PROJECT_DIRS.cache_dir();
+    fs::create_dir_all(cache).expect("Failed to create cache dir");
+    cache.to_path_buf()
+});
